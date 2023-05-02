ea97cbcd993cbc24981b353551a38ec7861aa278
sejin0211 <sejin0211@naver.com>
2023-05-02 13:17:24 +0900

    [문제3] 사각형의 넓이와 둘레 변수 width와 height를 선언하고 면적과 둘레를 구함

--- src/main/java/com/ohgiraffers/level01/basic/Application2.java
+++ src/main/java/com/ohgiraffers/level01/basic/Application2.java
@@ -3,7 +3,7 @@ package com.ohgiraffers.level01.basic;
 public class Application2 {
 
 	public static void main(String[] args) {
-		
+
 		/* 사각형의 넓이와 둘레를 구하는 문제이다.
 		 * 너비 12.5 높이 36.4를 변수에 저장하고
 		 * 각 넓이와 둘레를 계산해 변수에 담고 아래와 같이 출력되도록 하시오
@@ -12,7 +12,11 @@ public class Application2 {
 		 * 면적 : 455.0
 		 * 둘레 : 97.8
 		 *  */
-		
+		float width = 12.5F;
+		float height = 36.4F;
+
+		System.out.printf("면적 : %.1f\n", width * height);
+		System.out.println("둘레 : " + 2 * (width + height));
 	}
 
 }
