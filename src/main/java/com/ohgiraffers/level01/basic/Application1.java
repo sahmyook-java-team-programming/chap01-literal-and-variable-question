c95f1863cd22ae868dabf8299e95e4a0fde0b08a
choivember <127364272+choivember@users.noreply.github.com>
2023-05-02 14:02:39 +0900

    Revert "Seungmin"

--- src/main/java/com/ohgiraffers/level01/basic/Application1.java
+++ src/main/java/com/ohgiraffers/level01/basic/Application1.java
@@ -2,8 +2,16 @@ package com.ohgiraffers.level01.basic;
 
 public class Application1 {
 
-	public static void main(String[] args) {
-		
+		public static void main(String[] args) {
+			int a;
+			int b;
+			a = 20;
+			b = 30;
+			System.out.println(a+b);
+			System.out.println(a-b);
+			System.out.println(a*b);
+			System.out.println(a/b);
+			System.out.println(a%b);
 		/* 정수형 변수 2개를 선언하여  각 20과 30으로 초기화 한 후
 		 * 두 수의 더하기, 빼기, 곱하기, 나누기, 나머지를 다음과 같이 출력하세요
 		 * 
