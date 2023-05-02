903e7a5ae62ad37c57d812f05867ede034d617bb
choivember <pocketmongyo@gmail.com>
2023-05-02 13:17:23 +0900

    [문제1] 사칙연산 출력 문제 해결
    사칙연산 출력 문제를 풀었는데 잘 모르겠음

--- src/main/java/com/ohgiraffers/level02/normal/Application1.java
+++ src/main/java/com/ohgiraffers/level02/normal/Application1.java
@@ -2,8 +2,16 @@ package com.ohgiraffers.level02.normal;
 
 public class Application1 {
 
-	public static void main(String[] args) {
-		
+		public static void main(String[] args) {
+			int a;
+			int b;
+			a = 20;
+			b = 30;
+			System.out.println(a+b);
+			System.out.println(a-b);
+			System.out.println(a*b);
+			System.out.println(a/b);
+			System.out.println(a%b);
 		/* 문자형 변수를 하나 선언 후 'a'를 대입한 후
 		 * 출력은 문자 'a'가 가지는 유니코드값을 출력하도록 하세요
 		 * 
